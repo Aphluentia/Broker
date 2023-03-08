de615180cec49a20fe9d74bee742bfa4dda15ac4
Pedro Marques <pedroagoncalvesmarques@ua.pt>
2023-03-08 19:20:48 +0000

    Adds Base SpringBoot Project #19

---
+++ CommunicationAPI/src/test/java/communication/api/CommunicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package communication.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
