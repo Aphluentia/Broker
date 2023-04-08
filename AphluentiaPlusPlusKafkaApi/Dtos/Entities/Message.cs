d81098cec822fae289c09ba7451e075f0588c1b6
Pedro Marques <pedroagoncalvesmarques@ua.pt>
2023-04-08 15:51:53 +0100

    AddsDotnet api

---
+++ AphluentiaPlusPlusKafkaApi/Dtos/Entities/Message.cs
@@ -0,0 +1,16 @@
+﻿using AphluentiaPlusPlusKafkaApi.Dtos.Enum;
+
+namespace AphluentiaPlusPlusKafkaApi.Dtos.Entities
+{
+    public class Message
+    {
+
+        public Enum.Action Action { get; set; }
+        public string SourceId { get; set; }
+        public string TargetId { get; set; }
+        public ModuleType SourceModuleType { get; set; }
+        public ModuleType TargetModuleType { get; set; }
+        public string? Section { get; set; }
+        public string Timestamp { get; set; }
+    }
+}
---
+++ AphluentiaPlusPlusKafkaApi/Dtos/Enum/Action.cs
@@ -0,0 +1,10 @@
+﻿namespace AphluentiaPlusPlusKafkaApi.Dtos.Enum
+{
+    public enum Action
+    {
+        Create_Connection = 0,
+        Close_Connection = 1,
+        Ping_Connection = 2,
+        Update_Section = 3
+    }
+}
